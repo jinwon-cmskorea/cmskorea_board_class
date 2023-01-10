e259df42a40641c610322e2578e370bc9d31c8a1
siverNa(sna) <69504543+siverNa@users.noreply.github.com>
2023-01-10 13:07:29 +0900

    Board Class - index 작업
    작업한 내용 :
    index.php
    비로그인 상태이면 login 페이지로, 로그인 상태이면 게시판 리스트로 이동하도록 설정

--- public/index.php
+++ public/index.php
@@ -2,4 +2,12 @@
 /*
  * 진입 페이지 최초 접근은 이 페이지를 통해 진입하여 로그인이 필요한경우 로그인 페이지로, 로그인이 이미 된 경우 리스트 페이지로 이동한다.
  */
-echo "CMSKorea Board Class Project!";
+session_start();
+include __DIR__ . '/AutoLoad.php'; 
+
+$auth = new Cmskorea_Board_Auth();
+if (!$auth->isLogin()) {
+    header('location: ../view/login.php');
+} else {
+    header('location: ../view/boardList.php');
+}
