2386c99cdc135fd868873e8814e5e1214e3a2e20
siverNa(sna) <69504543+siverNa@users.noreply.github.com>
2023-01-11 13:13:25 +0900

    Board Class - 회원가입 페이지
    작업한 내용 :
    signup.php
    필수 항목 입력했는지 체크,
    필수 항목, 아이디 중복 체크 모두 통과했으면 가입 버튼 활성화, 그 외는 비활성화
    
    작업할 내용 :
    로그아웃 페이지 제작

--- public/view/signup.php
+++ public/view/signup.php
@@ -11,6 +11,8 @@
     <title>회원가입</title>
     <script type="text/javascript">
         $(document).ready(function() {
+            //id 중복 체크 변수
+            var idOk = false;
             //아이디를 입력받는 #inputId 에서 keyup 이벤트 발생 시
             $("#inputId").on("keyup", function() {
                 var self = $(this);
@@ -25,12 +27,34 @@
                         if (receive.status == 1) {
                             alert("중복된 아이디입니다.");
                             self.focus();
-                            $(".submit-btn").attr("disabled", true);
+                            idOk = false;
                         } else {
-                            $(".submit-btn").attr("disabled", false);
+                            idOk = true;
                         }
                     }
                 });
+            });
+            
+            //입력값이 모두 입력됐는지 확인하기 위한 변수
+            var isEmpty = true;
+            //각 input 이 변경되었을 때 실행
+            $("#signUpForm").find("input[type=text], input[type=password]").change(function() {
+                var Elements = $("#signUpForm input[type=text], #signUpForm input[type=password]");
+                for (var i = 0; i < Elements.length; i++)
+                {
+                    //비어있지 않으면 false 입력, 비어있으면 true 입력
+                    if ($(Elements[i]).val() != "" && $(Elements[i]).val() != null) {
+                        isEmpty = false;
+                    } else {
+                        isEmpty = true;
+                        break;
+                    }
+                }
+                //입력값이 모두 들어있고, 아이디 중복체크를 통과했으면 제출 비활성화 해제
+                if (!isEmpty && idOk)
+                    $(".submit-btn").attr("disabled", false);
+                else
+                    $(".submit-btn").attr("disabled", true);
             });
         });
     </script>
@@ -41,7 +65,7 @@
             <div class="signup-title">
                 <strong>씨엠에스코리아 게시판</strong><small class="subtitle">- 회원가입 -</small>
             </div>
-            <form class="form-horizontal" action="../process/signupOk.php" method="post">
+            <form id="signUpForm" class="form-horizontal" action="../process/signupOk.php" method="post">
                 <div class="form-group">
                     <label for="inputId" class="col-sm-2 category-design">아이디</label>
                     <div class="col-sm-10">
@@ -67,7 +91,7 @@
                     </div>
                 </div>
                 <div class="signup-button">
-                    <input type="submit" class="submit-btn" value="가  입">
+                    <input type="submit" class="submit-btn" value="가  입" disabled>
                     <input type="button" class="cancle-btn" onclick="location.href='./login.php';" value="취  소">
                 </div>
             </form>
