8e1314b8433207770fe4dc9e6017a57ee0d50518
siverNa(sna) <69504543+siverNa@users.noreply.github.com>
2023-01-11 14:05:10 +0900

    Board Class - 로그아웃 페이지
    작업한 내용 :
    logout.php, style.css
    로그아웃 페이지 디자인
    
    작업할 내용 :
    3초 후 이동 기능 추가

---
+++ public/view/logout.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/bootstrap/css/bootstrap.css" type="text/css">
+    <link rel="stylesheet" href="../css/style.css" type="text/css">
+    <script src="../css/bootstrap/js/bootstrap.js" type="javascript"></script>
+    <title>로그아웃</title>
+</head>
+<body>
+    <div class="container center">
+        <div class="col-sm-6" style="background-color: rgb(231, 230, 230);">
+            <div class="text-center">
+                <div class="sep-line">
+                   <strong>CMSKOREA Board</strong>
+                </div>
+            </div>
+            <div class="text-center logout-text">
+            	로그아웃 되었습니다.
+            </div>
+            <div class="containerFooter">
+                <div class="threeMsg">3초 후 처음 화면으로 이동합니다...</div>
+                <input type="button" class="logoutOk-btn" value="확인" onclick="location.href='../index.php'">
+            </div>
+        </div>
+    </div>
+</body>
+</html>
