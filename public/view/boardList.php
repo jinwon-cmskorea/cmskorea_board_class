77bff3e2998f0b4f11001b81cfe2e223f61b5362
siverNa(sna) <69504543+siverNa@users.noreply.github.com>
2023-01-11 15:54:04 +0900

    Board Class - 게시글 리스트 작업
    작업한 내용 :
    boardList.php, style.css
    검색 창 라인 디자인

--- public/view/boardList.php
+++ public/view/boardList.php
@@ -25,5 +25,35 @@
 </head>
 <body>
     <?php include_once __DIR__ . '/commonHeader.php';?>
+    <div class="col-sm-12">
+        <div class="list-title">
+            <strong style="color: rgb(89, 89, 89);">씨엠에스코리아 게시판</strong>
+            <small style="color: rgb(132, 151, 176);">- 리스트 -</small>
+        </div>
+        <div class="col-sm-12 list-descript">
+            등록 된 게시글을 조회하는 페이지입니다.<br>
+            등록 된 글은 조회, 수정, 삭제할 수 있습니다.
+        </div>
+    </div>
+    <!-- 상단 끝 -->
+    <!-- 검색, 작성, 게시글 리스트, 페이징 등  -->
+    <div class="col-sm-12 list-body">
+        <div class="board-upper">
+            <form action="./boardList.php" method="get">
+                <input type="hidden" name="page" value="1" />
+                <select class="selectbox" id="category" name="category">
+                    <option value="writer" <?php //echo (isset($category) && $category == 'writer') ? 'selected' : ''; ?>>작성자</option>
+                    <option value="title" <?php //echo (isset($category) && $category == 'title') ? 'selected' : ''; ?>>제목</option>
+                    <option value="insertTime" <?php //echo (isset($category) && $category == 'insertTime') ? 'selected' : ''; ?>>작성일자</option>
+                </select>
+                <input class="s-input" type="text" name="search" autocomplete="off" value="<?php //echo (isset($search) && $search) ? $search : ''; ?>">
+                <input class="btn s-button" type="submit" value="검색">
+            </form>
+            <div class="searchCnt"> 00 / 1,000 건</div>
+            <div>
+                <input class="btn bg-primary write-btn" type="button" onclick="location.href='./writeBoard.php';" value="작    성">
+            </div>
+        </div>
+    </div>
 </body>
 </html>
