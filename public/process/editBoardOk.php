f12ecaf9e50db48c74bb7c1f3fbf430c810ffea7
siverNa(sna) <69504543+siverNa@users.noreply.github.com>
2023-01-18 17:50:38 +0900

    Board Class - 게시글 수정 페이지
    작업한 내용 :
    editBoardOk.php
    잘못된 파일이 포함된 경우, 업로드 되지않도록 전처리 로직 추가,
    수정시 문제가 발생한 경우, 어떤 문제인지 출력되도록 수정,
    파일 검사를 통과한 경우에 게시글 수정 메소드가 동작하도록 수정

--- public/process/editBoardOk.php
+++ public/process/editBoardOk.php
@@ -23,58 +23,74 @@ $editDatas = array(
     'content'   => $_POST['content']
 );
 
-//받아온 값들을 토대로 게시글 수정, 문제가 있을 경우 exception 출력
-try {
-    $editRes = $board->editContent($editDatas);
-} catch (Exception $e) {
-    echo "<script type='text/javascript'>alert('{$e->getMessage()}')</script>";
-    echo "<script type='text/javascript'>history.back(-1)</script>";
-}
+//업로드한 파일에 문제가 있을 경우, 출력할 에러문 생성
+$fileStatus = array(
+    '1'         => '허용되지 않은 확장자의 파일입니다.',
+    '2'         => '파일의 용량이 너무 큽니다.'
+);
+//파일 정보를 담을 배열
+$editFileArrays = array();
+//허용된 확장자들이 포함되어있는 배열 생성
+$allowFiles = array(
+    'jpeg', 'jpg', 'gif', 'png', 'pdf'
+);
 
-$uploadStatus1 = true;
-$uploadStatus2 = true;
+/**
+ * 파일 검사 통과유무를 저장하는 변수
+ * Default 상태는 true, 문제가 있으면 false로 바뀜
+ */
+$uploadStatus = true;
 
-//파일이 존재하는 지 확인
-$fileArrays = $board->getFiles($editDatas['no']);
-
-//업로드 파일이 존재하면 파일 업로드 메소드 호출 및 테이블에 삽입
-if (isset($_FILES['inputFile1']['name']) && $_FILES['inputFile1']['name']) {
-    $file1 = $_FILES['inputFile1'];
-    
-    //만약 첨부파일 갯수가 이미 2개이면 가장 오래된 파일 삭제 후 새 파일 삽입
-    if (count($fileArrays) >= 2) {
-        $res = $board->delFile($fileArrays[0]['pk']);
-        if (!$res) {
-            echo "<script type=\"text/javascript\">alert('기존 파일 삭제 중 문제가 발생했습니다.')</script>";
+for ($i = 1; $i <= count($_FILES); $i++) {
+    $status = 0;
+    if (isset($_FILES['inputFile'.$i]['name']) && $_FILES['inputFile'.$i]['name']) {
+        $file = $_FILES['inputFile'.$i];
+        
+        $fileType = explode('/', $file['type']);
+        if (!in_array($fileType[1], $allowFiles)) { //파일 업로드 시 허용된 mime(jpg 등) 타입이 아니면 false 반환
+            $status = 1;
+        } else if ($file['size'] > 3145728) {//파일 업로드 시 3MB 초과하면 false 반환
+            $status = 2;
+        }
+        //파일 검사시, 에러가 있으면 에러문 리턴
+        if ($status) {
+            $uploadStatus = false;
+            echo "<script type=\"text/javascript\">alert('{$fileStatus[$status]}')</script>";
             echo "<script type=\"text/javascript\">history.back(-1)</script>";
+            exit;
+        } else { //통과한 경우, content를 담은 배열을 추가
+            $file['content'] = file_get_contents($file['tmp_name']);
+            array_push($editFileArrays, $file);
         }
     }
-    $file1['content'] = file_get_contents($file1['tmp_name']);
-    $uploadStatus1 = $board->addFile($editDatas['no'], $file1);
 }
 
-//파일이 존재하는 지 확인
-$fileArrays = $board->getFiles($editDatas['no']);
-
-//업로드 파일이 존재하면 파일 업로드 메소드 호출 및 테이블에 삽입
-if (isset($_FILES['inputFile2']['name']) && $_FILES['inputFile2']['name']) {
-    $file2 = $_FILES['inputFile2'];
-    
-    //만약 첨부파일 갯수가 이미 2개이면 가장 오래된 파일 삭제 후 새 파일 삽입
-    if (count($fileArrays) >= 2) {
-        $res = $board->delFile($fileArrays[0]['pk']);
-        if (!$res) {
-            echo "<script type=\"text/javascript\">alert('기존 파일 삭제 중 문제가 발생했습니다.')</script>";
-            echo "<script type=\"text/javascript\">history.back(-1)</script>";
+/*
+ * 업로드 파일이 이상 없으면 게시글 생성 후 파일 업로드, 성공 메세지 출력
+ * 실패시 뒤로가기
+ */
+if ($uploadStatus) {
+    //게시글 작성 코드. 성공하면 게시글 번호를 리턴받고, 실패하면 설정된 예외문 호출 뒤 뒤로 감
+    try {
+        //게시글 작성 메소드 호출
+        $editRes = $board->editContent($editDatas);
+        
+        //새로 업로드한 파일이 존재할 경우, 로직 수행
+        for ($i = 0; $i < count($editFileArrays); $i++) {
+            $fileArrays = $board->getFiles($editDatas['no']);
+            //만약 첨부파일 갯수가 이미 2개이면 가장 오래된 파일 삭제 후 새 파일 삽입
+            if (count($fileArrays) >= 2) {
+                $res = $board->delFile($fileArrays[0]['pk']);
+                if (!$res) {
+                    echo "<script type=\"text/javascript\">alert('기존 파일 삭제 중 문제가 발생했습니다.')</script>";
+                    echo "<script type=\"text/javascript\">history.back(-1)</script>";
+                }
+            }
+            $board->addFile($editDatas['no'], $editFileArrays[$i]);
         }
+        echo "<script>location.href='../view/boardList.php?message=editSuccess'</script>";
+    } catch (Exception $e) {
+        echo "<script type=\"text/javascript\">alert('{$e->getMessage()}')</script>";
+        echo "<script type=\"text/javascript\">history.back(-1)</script>";
     }
-    $file2['content'] = file_get_contents($file2['tmp_name']);
-    $uploadStatus2 = $board->addFile($editDatas['no'], $file2);
-}
-
-if ($editRes && $uploadStatus1 && $uploadStatus2) {
-    echo "<script type=\"text/javascript\">location.href='../view/boardList.php?message=editSuccess'</script>";
-} else {
-    echo "<script type=\"text/javascript\">alert('게시글 수정 중 문제가 발생했습니다.')</script>";
-    echo "<script type=\"text/javascript\">history.back(-1)</script>";
 }
