41b05928006fe151dd742f4257e7de5ccf0b4186
siverNa(sna) <69504543+siverNa@users.noreply.github.com>
2023-01-18 11:22:28 +0900

    Board Class - 게시글 삭제 작업
    작업한 내용 :
    deleteOk.php
    게시글 삭제 시, 파일 삭제가 가 안되는 현상 발견,
    파일이 존재하는 경우, 파일을 삭제 후 게시글을 삭제하도록 구현

--- public/process/deleteOk.php
+++ public/process/deleteOk.php
@@ -13,6 +13,22 @@ if (isset($_POST['pk']) && $_POST['pk']) {
 }
 
 if (isset($pk) && $pk) {
+    //게시글을 삭제 전에, 게시글에 첨부된 파일이 존재하는 경우 파일부터 먼저 삭제
+    $fileArrays = $board->getFiles($pk);
+    if (count($fileArrays) > 0) {
+        for ($i = 0; $i < count($fileArrays); $i++) {
+            $fileDelRes = $board->delFile($fileArrays[$i]['pk']);
+            
+            //파일 삭제가 정상적을 되지 않았으면 게시물 삭제하면 안됨. 에러가 있다고 return
+            if (!$fileDelRes) {
+                $status = 0;
+                $receive = array('status' => $status);
+                echo json_encode($receive, JSON_UNESCAPED_UNICODE);
+                return ;
+            }
+        }
+    }
+    
     $delRes = $board->delContent($pk);
     if ($delRes) {
         $status = 1;
